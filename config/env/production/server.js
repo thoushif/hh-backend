db1ec5b8007ec77cd435af931fe542853acb17e9
Thoushif Aazam Shaik <aazam555@gmail.com>
2022-02-21 14:54:16 -0600

    v4 server.js for heroku

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,10 @@
 module.exports = ({ env }) => ({
   url: env("HEROKU_URL"),
+  proxy: true,
+  app: {
+    keys: env.array("APP_KEYS", [
+      "DATABASE_URL",
+      "HEROKU_POSTGRESQL_MAROON_URL",
+    ]),
+  },
 });
